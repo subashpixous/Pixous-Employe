bb15f646a237a6dcd994c6bddc180d9e48f27311
subashpixous <subash.pixous@gmail.com>
2026-05-07 11:19:03 +0530

    committe

--- helpers/functions.php
+++ helpers/functions.php
@@ -11,15 +11,6 @@ function startSecureSession(): void
         ini_set('session.use_strict_mode', '1');
         ini_set('session.cookie_httponly', '1');
 
-        session_set_cookie_params([
-            'lifetime' => 0,
-            'path' => '/',
-            'domain' => '',
-            'secure' => true,
-            'httponly' => true,
-            'samesite' => 'Lax'
-        ]);
-
         session_start();
     }
 
